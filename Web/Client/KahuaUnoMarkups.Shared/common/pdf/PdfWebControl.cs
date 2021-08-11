1beba8b8d797d5de55567d4272411fdc6a0cd5d1
Brooks Lindsey <BLindsey@Kahua.com>
2021-08-11 13:59:37 -0400

    Added web pdfviewer projects

---
+++ Web/Client/KahuaUnoMarkups.Shared/common/pdf/PdfWebControl.cs
@@ -0,0 +1,114 @@
+﻿#if __WASM__
+using System;
+using System.Collections.Generic;
+using System.Text;
+//using kahua.kdk.utility;
+using Uno.Foundation;
+using Uno.UI.Runtime.WebAssembly;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+
+namespace kahua.host.uno.common.pdf
+{
+    public class PdfWebControl : Control
+    {
+        private string _folderPath;
+
+        public PdfWebControl()
+        {
+            Console.WriteLine($"{nameof(PdfWebControl)}.ctor");
+
+            Background = new SolidColorBrush(Colors.Transparent);
+            Loaded += PdfWebControl_Loaded;
+        }
+
+        private void PdfWebControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine($"{nameof(PdfWebControl)}.PdfWebControl_Loaded");
+
+            var htmlId = $"PDF_{this.GetHtmlId()}";
+            this.SetHtmlContent($@"<div id=""{htmlId}""></div>");
+        }
+
+        public async void ImportXfdf()
+        {
+            var fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            fileOpenPicker.FileTypeFilter.Add("*");
+            StorageFile pickedFile = await fileOpenPicker.PickSingleFileAsync();
+            if (pickedFile != null)
+            {
+                var name = pickedFile.Path;
+                var htmlId = $"PDF_{this.GetHtmlId()}";
+
+                var fullPath = System.IO.Path.Combine(_folderPath, name);
+
+                string javascript = $@"
+                (function(){{
+                    var pdfviewer = (document.getElementById('{htmlId}')).ej2_instances[0];    
+                    pdfviewer.importAnnotation('{fullPath}');
+                }})();";
+                this.ExecuteJavascript(javascript);
+            }
+            else
+            {
+                // No file was picked or the dialog was cancelled.
+            }
+        }
+
+        public void ExportXfdf()
+        {
+            var htmlId = $"PDF_{this.GetHtmlId()}";
+
+            string javascript = $@"
+                (function(){{
+                    var pdfviewer = (document.getElementById('{htmlId}')).ej2_instances[0];    
+                    pdfviewer.exportAnnotation('Xfdf');
+                }})();";
+            this.ExecuteJavascript(javascript);
+        }
+
+        public bool LoadPdfViewer(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                this.ExecuteJavascript("alert('Please enter a path value');");
+                return false;
+            }
+
+            _folderPath = path;
+            Console.WriteLine($"{nameof(PdfWebControl)}.TryLoadDocument");
+
+            var htmlId = $"PDF_{this.GetHtmlId()}";
+
+#if RELEASE
+            var serviceUrl = "https://titan.kahua.com/webmarkupservice01/api/pdfviewer";
+#else
+            var serviceUrl = "http://localhost:50801/api/pdfviewer";
+#endif
+            //serviceUrl = "https://ej2services.syncfusion.com/production/web-services/api/pdfviewer";
+            //var documentPath = "https://www.clickdimensions.com/links/TestPDFfile.pdf";
+
+            string javascript = $@"
+                (function(){{
+                    // initialize PDF Viewer component.
+                    ej.pdfviewer.PdfViewer.Inject(ej.pdfviewer.Toolbar, ej.pdfviewer.Annotation, ej.pdfviewer.TextSelection, ej.pdfviewer.TextSearch, ej.pdfviewer.Navigation, ej.pdfviewer.Print);
+
+                    var pdfviewer = new ej.pdfviewer.PdfViewer({{
+                    serviceUrl: '{serviceUrl}'
+                    }});
+
+                    pdfviewer.appendTo('#{htmlId}');
+                }})();";
+            this.ExecuteJavascript(javascript);
+
+            return true;
+        }
+    }
+}
+#endif
